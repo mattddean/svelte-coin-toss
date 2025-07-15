16a81de1135896cc53da46271f187d37a642e910
Matt Dean <mdean400@gmail.com>
2025-07-14 22:19:59 -0400

    initial commit

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  interface Props {
+    count: number
+  }
+
+  let { count = $bindable() }: Props = $props()
+</script>
+
+<button onclick={() => count++}>
+  count is {count}
+</button>
+
+<style>
+  button {
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 0.6em 1.2em;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    background-color: #1a1a1a;
+    color: white;
+    cursor: pointer;
+    transition: border-color 0.25s;
+  }
+  button:hover {
+    border-color: #646cff;
+  }
+  button:focus,
+  button:focus-visible {
+    outline: 4px auto -webkit-focus-ring-color;
+  }
+</style>
